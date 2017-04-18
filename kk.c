84807d1743f3cf9d106827eef8572e2a1c27929b
Niamh Mulholland <niamhmulholland@college.harvard.edu>
2017-04-18 10:54:43 -0400

    standard rep implementation

--- kk.c
+++ kk.c
@@ -4,12 +4,63 @@
 * Chris Arndt & Niamh Mulholland
 * */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <time.h>
+#include <math.h>
+
 #define MAX_ITER 100
 
-int num[10] = {2,8,7,6,0,2,8,7,6,0};
-int* sol;
+/* 
+ * Standard representation functions
+ *
+ *  standard_rep - generates random sequence of -1,1 
+ *  neighbours - moves from solution S to a neighbour
+ * 				 by moving 1 or 2 elements from set A1 
+ *				 to A2 and vice versa, or swapping elements
+ *				 from two sets,
+ */
 
-sol = num;
+int* standard_rep(int n) {
+	int* sol = malloc(sizeof(int)*n);
+	for(int i=0; i<n; i++){
+		double num = (drand48()*2 - 1);
+		int numf = num/(fabs(num));
+		sol[i] = numf;
+	}
+return sol;
+}
+
+
+int* neighbours(int sol[], int n) {
+	int prob_swap = drand48();
+
+	if(prob_swap < 0.5) {
+		int i1 = rand()%(n-1);
+		int i2 = rand()%(n-1);
+		int temp = sol[i1];
+		sol[i1] = sol[i2];
+		sol[i2] = temp;
+	}
+	else {
+		int prob_elt = drand48();
+		//  (100/(100 choose 2))
+		int prob_set = 2/99;
+
+		if(prob_elt < prob_set){
+			int i = (rand()%(n-1));
+			sol[i] = sol[i]*(-1);
+		}
+		else {
+			int i1 = rand()%(n-1);
+			int i2 = rand()%(n-1);
+			sol[i1] = sol[i1]*(-1);
+			sol[i2] = sol[i2]*(-1);
+		}
+	}
+return sol;
+}
 
 /* repeated random
 * takes pointer to list
@@ -24,6 +75,7 @@ sol = num;
 * IMPLEMENT T()
 */
 
+/*
 int* repeated_rand(int* sol) {
 	for(int i = 0; i < MAX_ITER; i++){
 		int* new_sol = rand_sol();
@@ -35,11 +87,11 @@ return sol;
 }
 
 
-/* hill climbing
+* hill climbing
 * takes pointer to list
 * improves through moves to (better) neighbours
 * returns residue 
-*/
+*
 
 int* hill_climb(int* sol) {
 	for(int i = 0; i < MAX_ITER; i++){
@@ -51,11 +103,11 @@ int* hill_climb(int* sol) {
 return sol;
 }
 
-/* simulated annealing 
+* simulated annealing 
 * takes pointer to list
 * improves through moves to (not always better) neighbours
 * returns residue 
-*/
+*
 
 int* sim_anneal(int* sol) {
 	int* orig_sol = sol;
@@ -71,5 +123,15 @@ int* sim_anneal(int* sol) {
 	     	}
 	     }
 return orig_sol;
-}
+}*/ 
 
+int main() { 
+	time_t t;
+	// initialise RNG
+	srand48(time(&t));
+	int* sol = standard_rep(10);
+	neighbours(sol, 10);
+	free(sol);
+
+
+}	
